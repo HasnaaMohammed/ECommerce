715bb071297c69eb7ce6280c10301904040648af
Mohamed.Fawzy <mohamedfawzy1993@gmail.com>
2018-02-28 17:25:47 +0200

    -

---
+++ src/main/java/controller/servlets/LoginServlet.java
@@ -0,0 +1,27 @@
+package controller.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/Login")
+public class LoginServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       resp.sendRedirect("./404.html");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    private void processRequest(HttpServletRequest request , HttpServletResponse response) throws IOException {
+        response.getWriter().print("Hello ^_^");
+    }
+
+}
